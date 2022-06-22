fea741b3f50a7e1de7f6d9cae78bd508a65f468d
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-22 16:13:48 +0300

    Completed the assignment.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,64 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+displayAllToys();
+addAnewToy();
 });
+
+//Get all toys and display them on page load
+function displayAllToys(){
+  fetch('http://localhost:3000/toys')
+  .then(response=>response.json())
+  .then(data=>{
+   data.forEach(toy => displayOneToy(toy));
+  })
+  .catch(error=>console.log(error))
+}
+
+//add an individual toy to the display
+function displayOneToy(toy){
+  const toyCollectionDiv = document.querySelector('#toy-collection');
+  const newToyCardDiv = document.createElement('div');
+  newToyCardDiv.classList.add('card');
+  newToyCardDiv.innerHTML = `<h2>${toy.name}</h2> <img class='toy-avatar' src=${toy.image}> <p>${toy.likes}</p> <button id=${toy.id} class='like-btn'>Like ❤️</button>`;
+  toyCollectionDiv.appendChild(newToyCardDiv);
+  increaseAToyLikes();
+}
+
+//add a new toy to the collection
+function addAnewToy(){
+document.getElementsByClassName('add-toy-form')[0].addEventListener('submit',function(event){
+event.preventDefault()
+const newToyDetails = document.getElementsByClassName('input-text');
+
+
+fetch('http://localhost:3000/toys',{
+  method:"POST",
+  headers:{
+    'Content-Type':'application/json',
+    Accept:'application/json'
+  },
+body:JSON.stringify({
+  name:newToyDetails[0].value,
+  image:newToyDetails[1].value,
+  likes:0
+})
+})
+.then(response=>response.json())
+.then(createdToy=>displayOneToy(createdToy))
+.catch(error=>console.log(error))
+});
+}
+
+//Increase the likes count and update in the DOM
+function increaseAToyLikes(){
+  const likeButtons = document.querySelectorAll('.like-btn') 
+  likeButtons.forEach(likeButton=>{
+  likeButton.addEventListener('click',function(){
+  let currentLikes = likeButton.parentNode.querySelector('p').textContent;
+  currentLikes++;
+  likeButton.parentNode.querySelector('p').textContent = currentLikes;
+  })
+  })
+}
